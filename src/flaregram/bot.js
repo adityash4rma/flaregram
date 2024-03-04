9d93990d75c35808dc3531eb7ae2d4b88e807308
adityash4rma <adityasharma13373@gmail.com>
2024-03-04 21:26:46 +0530

    add forwardMessages function

--- src/flaregram/bot.js
+++ src/flaregram/bot.js
@@ -2,6 +2,7 @@
 //// flaregram © 2024 by Aditya Sharma is licensed under Attribution-NonCommercial 4.0 International. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
 
 import {f_forwardMessage} from "./api/func_forwardMessage";
+import {f_forwardMessages} from "./api/func_forwardMessages";
 import {f_sendMessage} from "./api/func_sendMessage";
 import { f_answerCallbackQuery } from "./api/func_answerCallbackQuery";
 
@@ -11,6 +12,7 @@ export const bot = {
     message: {
         sendMessage: f_sendMessage,
         forwardMessage: f_forwardMessage,
+        forwardMessages: f_forwardMessages,
         answerCallbackQuery: f_answerCallbackQuery
     },
 
